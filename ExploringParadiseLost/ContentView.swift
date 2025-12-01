f78c664b67341ae6b9d55e700fb592fbcb12d324
vito4488778-arch <vito.4488778@gmail.com>
2025-12-02 00:45:02 +0800

    Initial Commit

---
+++ ExploringParadiseLost/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExploringParadiseLost
+//
+//  Created by 114iosClassStudent04 on 2025/12/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExploringParadiseLost/ExploringParadiseLostApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringParadiseLostApp.swift
+//  ExploringParadiseLost
+//
+//  Created by 114iosClassStudent04 on 2025/12/2.
+//
+
+import SwiftUI
+
+@main
+struct ExploringParadiseLostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
